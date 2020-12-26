fa663b4b9bbbf33d9f0d3f5119f3f9eee9706553
Dedines <tipaev_98@mail.ru>
2020-12-27 01:25:11 +0400

    Added graphics for minimum temperature

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
 import requests
+import re
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
 from sqlalchemy import create_engine, Table, Column, String, Float, MetaData
 from sqlalchemy.sql import select
 
@@ -47,7 +51,45 @@ metadata.create_all(engine)
 c = engine.connect()
 
 provider = WeatherProvider('EWLYGPYQPMM6CASTQXW4YH9KU')
-c.execute(weather.insert(), provider.get_data('Volgograd,Russia', '2020-09-20', '2020-09-29'))
+c.execute(weather.insert(), provider.get_data('Volgograd,Russia', '2020-12-01', '2020-12-31'))
 
-for row in c.execute(select([weather])):
+for row in c.execute('SELECT * FROM weather'):
     print(row)
+
+#построение графика температуры
+fig, ax = plt.subplots()
+ax.set(title='График изменения минимальной температуры за декабрь')
+
+ax.grid(which='major',
+        color = 'k')
+
+mint_y =[]
+mint_x = []
+
+#делаем выборку минимальной температуры из БД
+for row in c.execute('SELECT date FROM weather'):
+    mint_x.append(row)
+
+#распаковываем список
+mint_x = [i for i, in mint_x]
+
+#делаем выборку дней из БД
+for row in c.execute('SELECT mint FROM weather'):
+    mint_y.append(row)
+
+#распаковываем список
+mint_y = [j for j, in mint_y]
+
+print(mint_y)
+print(mint_x)
+
+#устанавливаем границы графика
+plt.xlim(0, len(mint_y))
+plt.ylim(min(mint_y), max(mint_y))
+
+#строим график
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.plot(mint_x, mint_y, color='r', linewidth=3)
+plt.gcf().autofmt_xdate()
+plt.show()
